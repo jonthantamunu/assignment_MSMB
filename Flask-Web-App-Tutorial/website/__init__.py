d0fc9335147d1db7dd6aa89a4ca3783be3d0b8c0
jonthantamunu <144085634+jonthantamunu@users.noreply.github.com>
2024-05-10 11:05:32 +0700

    Update __init__.py

--- Flask-Web-App-Tutorial/website/__init__.py
+++ Flask-Web-App-Tutorial/website/__init__.py
@@ -4,13 +4,13 @@ from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = "postgresql://postgres:123456@localhost/msmb"
+DB_NAME = "postgresql://jonathan:123456@localhost/msmb"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/msmb'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'jonathan://postgres:123456@localhost/msmb'
 
     db.init_app(app)
 
